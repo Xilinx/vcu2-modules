a0b6bd20dfd986a0877e75ff86effe422be26d42
Yassine Ouaissa <yassine.ouaissa@allegrodvt.com>
2024-09-24 09:15:41 +0200

    fix: change copyright notice.
    The xilinx al_riscv_xxx drivers should have GPL v2 licence.
    
    Signed-off-by: Yassine Ouaissa <yassine.ouaissa@allegrodvt.com>

--- al_riscv/al_dmabuf.c
+++ al_riscv/al_dmabuf.c
@@ -1,8 +1,18 @@
-/******************************************************************************
-*
-* Copyright (C) 2019 Allegro DVT2.  All rights reserved.
-*
-******************************************************************************/
+/*
+ * Copyright (C) 2024, Allegro DVT2 (www.allegrodvt.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "al_dmabuf.h"
 
--- al_riscv/al_riscv_wrap_dec.c
+++ al_riscv/al_riscv_wrap_dec.c
@@ -1,7 +1,20 @@
 /*
-* 
-* Copyright (C) 2019 Allegro DVT2.  All rights reserved.
-*/
+ * al_riscv_wrap_dec.c userspace wrapper for the decoder driver. 
+ *
+ * Copyright (C) 2024, Allegro DVT2 (www.allegrodvt.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <linux/of.h>
 #include <linux/platform_device.h>
@@ -34,7 +47,7 @@ static struct platform_driver al_riscv_dec_driver = {
 
 module_platform_driver(al_riscv_dec_driver);
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
 MODULE_ALIAS("platform:ald3_riscv");
 MODULE_AUTHOR("Mickael Guene <mickael.guene@allegrodvt.com>");
 MODULE_DESCRIPTION("Allegro DVT RiscV decoder driver");
